56df54e302618ebc7e15a838d1f4a27c4e7da13b
chayliu <chayliu@126.com>
2021-08-17 11:26:25 +0800

    update chapter 63

---
+++ code/altio/tty_functions.h
@@ -0,0 +1,10 @@
+#ifndef TTY_FUNCTIONS_H
+#define TTY_FUNCTIONS_H
+
+#include <termios.h>
+
+int ttySetCbreak(int fd, struct termios *prevTermios);
+
+int ttySetRaw(int fd, struct termios *prevTermios);
+
+#endif
